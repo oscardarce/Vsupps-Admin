58824fd489c00fcd25f5c9f570c49ca14d22395d
Oscar Darce <oscardarce@gmail.com>
2023-08-16 17:19:38 -0600

    16/8

--- app/api/[storeId]/sizes/[sizeId]/route.ts
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -27,7 +27,7 @@ export async function GET(
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { sizeId: string; storeId: string } }
+  { params }: { params: { storeId: string; sizeId: string } }
 ) {
   try {
     const { userId } = auth();
