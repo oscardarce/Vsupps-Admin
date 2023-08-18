c0d1e5bf68022d8da1914bba915ba985af8a55c3
oscardarce <oscardarce@gmail.com>
2023-08-17 23:49:04 -0600

    17/8

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export const formatter = new Intl.NumberFormat("es-CR", {
+  style: "currency",
+  currency: "CRC",
+});
